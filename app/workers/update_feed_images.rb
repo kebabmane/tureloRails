458308db00b013a8f45a2f3787cee707f6a30570
Rhys Evans <rjevans@Rhyss-MacBook.local>
2016-04-19 20:21:17 +1000

    updated from rake task to background worker

---
+++ app/workers/update_feed_images.rb
@@ -0,0 +1,31 @@
+class FeedEntryCronWorker
+  include Sidekiq::Worker
+
+  def perform
+
+FeedEntry.find_each do |feed_entry|
+
+@feed_entry = FeedEntry.find(feed_entry.id)
+
+ doc = Nokogiri::HTML(@feed_entry.summary)
+ img_srcs = doc.css('img').map{ |i| i['src'] }
+ img_srcs.each do |url|
+   FeedEntryImage.create!(
+      :img_url              => url,
+      :feed_entry_id        => @feed_entry.id,
+   )
+ end
+
+
+ doc = Nokogiri::HTML(@feed_entry.feed_entry_content)
+ img_srcs = doc.css('img').map{ |i| i['src'] }
+ img_srcs.each do |url|
+   FeedEntryImage.create!(
+      :img_url              => url,
+      :feed_entry_id        => @feed_entry.id,
+   )
+end
+end
+
+end
+end
