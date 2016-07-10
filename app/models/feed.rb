035fa5be1cedc7ac98509351f60cf3bd30ffac2f
Rhys Evans <rjevans@Rhyss-MacBook.local>
2016-07-10 10:28:34 +1000

    fixed the slugging issues

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,15 +1,16 @@
 class Feed < ActiveRecord::Base
   extend FriendlyId
+  friendly_id :feed_name, use: [:slugged, :finders]
   searchkick match: :word_start, searchable: [:feed_name], callbacks: :async, track: true, conversions: "conversions"
   has_paper_trail
   acts_as_paranoid
   acts_as_taggable
   acts_as_followable
   is_impressionable :counter_cache => true, :unique => true
-  friendly_id :feed_name, use: :slugged
-
-  has_many :feed_entries, dependent: :destroy
 
   validates :feed_url, presence: true, uniqueness: true
+  validates :feed_name, :slug, presence: true
+
+  has_many :feed_entries, dependent: :destroy
 
 end
