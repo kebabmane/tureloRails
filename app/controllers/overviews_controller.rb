f49944d0ad16b4b03620b4fb73ba8a6adc57bd2e
Rhys Evans <rjevans@Rhyss-MacBook.local>
2016-07-15 15:39:15 +1000

    more

--- app/controllers/overviews_controller.rb
+++ app/controllers/overviews_controller.rb
@@ -10,7 +10,6 @@ class OverviewsController < ApplicationController
      @feed_entries = FeedEntry.preload(:feed).includes(:feed_entry_images).where(feed_id: current_user.followees(Feed)).order("published DESC").page(params[:page]).per_page(9)
   end
 
-
   respond_to do |format|
     format.html
     format.js
