d3df7464d902af53a99e64bd0dcdf51bfa323d17
Rhys Evans <rjevans@Rhyss-MacBook.local>
2016-08-13 18:17:15 +1000

    more updates to sidekiq.yml

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,11 @@
+schedule_file = "config/schedule.yml"
+
+if File.exists?(schedule_file) && Sidekiq.server?
+  Sidekiq::Cron::Job.load_from_hash! YAML.load_file(schedule_file)
+end
+
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV["REDIS_URL"], namespace: :resque, :size => 7 }
-
-  schedule_file = "config/schedule.yml"
-
-  if File.exists?(schedule_file) && Sidekiq.server?
-    Sidekiq::Cron::Job.load_from_hash! YAML.load_file(schedule_file)
-  end
 end
 
 
