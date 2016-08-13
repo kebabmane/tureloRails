37c1020cae7fd73c8a45751f7e8527956d362df7
Rhys Evans <rjevans@Rhyss-MacBook.local>
2016-08-13 19:46:28 +1000

    updates

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,14 +1,16 @@
-schedule_file = "config/schedule.yml"
 
-if File.exists?(schedule_file) && Sidekiq.server?
-  Sidekiq::Cron::Job.load_from_hash! YAML.load_file(schedule_file)
-end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV["REDIS_URL"], namespace: :resque, :size => 7 }
+  config.redis = { url: ENV["REDIS_URL"], namespace: "turelo-production", :size => 12 }
+
+  schedule_file = "config/schedule.yml"
+
+  if File.exists?(schedule_file) && Sidekiq.server?
+    Sidekiq::Cron::Job.load_from_hash! YAML.load_file(schedule_file)
+  end
 end
 
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV["REDIS_URL"], namespace: :resque, :size => 13 }
+  config.redis = { url: ENV["REDIS_URL"], namespace: "turelo-production", :size => 8 }
 end
