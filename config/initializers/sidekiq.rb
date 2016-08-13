2268d8eee5c0f2aa7f840b3215489a93423ff2fb
Rhys Evans <rjevans@Rhyss-MacBook.local>
2016-08-13 17:12:31 +1000

    fixed schedules

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,14 +1,14 @@
-schedule_file = "config/schedule.yml"
-
-if File.exists?(schedule_file) && Sidekiq.server?
-  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
-end
-
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV["REDIS_URL"], namespace: :resque, :size => 1 }
+  config.redis = { url: ENV["REDIS_URL"], namespace: :resque, :size => 7 }
+
+  schedule_file = "config/schedule.yml"
+
+  if File.exists?(schedule_file) && Sidekiq.server?
+    Sidekiq::Cron::Job.load_from_hash! YAML.load_file(schedule_file)
+  end
 end
 
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV["REDIS_URL"], namespace: :resque, :size => 7 }
+  config.redis = { url: ENV["REDIS_URL"], namespace: :resque, :size => 13 }
 end
