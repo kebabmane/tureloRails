57f5c43e96c20abbf2c7cb97809a54b2ce4176eb
Rhys Evans <rjevans@Rhyss-MacBook.local>
2016-08-28 14:13:33 +1000

    small clean up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,6 +16,10 @@ module ApplicationHelper
       { success: "alert-success", error: "alert-error", alert: "alert-warning", notice: "alert-info" }[flash_type.to_sym] || flash_type.to_s
   end
 
+  def followed_feeds
+    @followed_feeds = current_user.followees(Feed)
+  end
+
   def flash_messages(opts = {})
     flash.each do |msg_type, message|
       concat(content_tag(:div, message, class: "make-visable alert flash-alert #{bootstrap_class_for(msg_type)} alert-dismissible", role: 'alert') do
