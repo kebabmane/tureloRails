bc8a6b005876cb34baaa8b1cf32bfccd681f3602
Rhys Evans <rjevans@Rhyss-MacBook.local>
2016-12-16 21:49:12 +1100

    moving over to the material framework from bootstrap

--- app/controllers/overviews_controller.rb
+++ app/controllers/overviews_controller.rb
@@ -6,11 +6,11 @@ class OverviewsController < ApplicationController
   order = params[:newest] ? {rank: :desc} : {published: :desc}
 
   if params[:search].present?
-     @feed_entries = FeedEntry.includes(:feed, :feed_entry_images, :impressions).all.order("published DESC").search(params[:search], suggest: true, track: true, page: params[:page], per_page: 9)
+     @feed_entries = FeedEntry.includes(:feed, :feed_entry_images, :impressions).all.order("published DESC").search(params[:search], suggest: true, track: true, page: params[:page], per_page: 10)
   elsif params[:tag].present?
-     @feed_entries = FeedEntry.tagged_with(params[:tag]).order("published DESC").page(params[:page]).per_page(9)
+     @feed_entries = FeedEntry.tagged_with(params[:tag]).order("published DESC").page(params[:page]).per_page(10)
   else
-     @feed_entries = FeedEntry.where(feed_id: current_user.followees(Feed)).order(order).includes(:feed, :feed_entry_images).page(params[:page]).per_page(9)
+     @feed_entries = FeedEntry.where(feed_id: current_user.followees(Feed)).order(order).includes(:feed, :feed_entry_images).page(params[:page]).per_page(10)
   end
 
   @votes = @feed_entries.includes(:votes).each_with_object({}) do |feed_entry, object|
