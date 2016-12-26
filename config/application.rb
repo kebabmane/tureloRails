bd3435d9f3d248bf4b00abac86de31c3ebe25364
Rhys Evans <evans.rhys@gmail.com>
2016-12-26 12:37:58 +1100

    fixing up some redis docker issues

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,11 @@ module Turelo
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    # config.cache_store = :redis_store, ENV['REDIS_URL'],{ namespace: 'turelo::cache' }
+
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.log_tags  = [:subdomain, :uuid]
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 end
